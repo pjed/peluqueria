3a3c50a8784419907dbb254680dbdac154fb8927
Pedro <espinosaduque@gmail.com>
2020-12-10 19:02:35 +0100

    Comprobar que existe en la BBDD el correo y la contraseña

--- app/Http/Controllers/controlador.php
+++ app/Http/Controllers/controlador.php
@@ -1,13 +1,14 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use App\Models\usuario;
 
-class controlador extends Controller
-{
-    
+class controlador extends Controller {
+
     /**
      * Cerrar sesion
      * @author Pedro
@@ -19,27 +20,32 @@ class controlador extends Controller
         session()->regenerate();
         return view('inicio');
     }
-    
+
     /**
      * Método para comprobar si un usuario existe en la BD para hacer login
      * @param type $correo email del usuario
      * @param type $pwd contraseña del usuario
      * @return type usuario
      */
-    static function inicioSesion($correo, $pwd) {
+    static function inicioSesion(Request $req) {
         $correo = $req->get('usuario');
         $pass = $req->get('pwd');
+
+        //Me traigo todos los usuarios 
+        $usuarios = usuario::all();
         
-        $user = usuario::all();
-        
-        dd($v);
+        //Compruebo que el usuario y la password coincide con algun 
+        //usuario de todos los que hay en la tabla usuarios
+
+        $usuario = $usuarios->where([
+                    ['EMAIL', '=', $correo],
+                    ['PASSWORD', '=', $pass]
+                ])->first();
+        dd($usuario);
         $rol = rol::all();
         $tiene = tiene::all();
 
-        $user->where([
-            ['EMAIL', '=', $correo],
-            ['PASSWORD', '=', $pass]
-        ])->get();
         return $v;
     }
+
 }
